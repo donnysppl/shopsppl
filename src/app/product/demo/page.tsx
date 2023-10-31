6c10a7edc3a530cfb41ec8eacce7f71de50c5cd7
donnysppl <donny@sppl.ind.in>
2023-10-31 12:19:54 +0530

    changes in product demo

--- src/app/product/demo/page.tsx
+++ src/app/product/demo/page.tsx
@@ -1,20 +1,7 @@
-import ProductListLayout from '@/components/layout/ProductListLayout'
 import React from 'react'
 
-export default function Demo({ searchParams }: {
-  searchParams: {
-    page: string, limit: string, brand: string, category: string
-  }
-}) {
-  const pageParams = parseInt(searchParams.page);
-  const limitParams = parseInt(searchParams.limit);
-  const brandParams = searchParams.brand;
-  const categoryParams = searchParams.category;
-
+export default function Demo() {
   return (
-    <>
-    <ProductListLayout page={pageParams}
-    limit={limitParams} brand={brandParams} category={categoryParams}  />
-    </>
+    <div>Demo</div>
   )
 }
